be77aa66cb60ab104713a2d510a7ce854809a6d6
hafizulf <hafizulf68@gmail.com>
2021-09-30 22:40:36 +0700

    feat(villages): read data

--- app/Controllers/Http/VillageController.js
+++ app/Controllers/Http/VillageController.js
@@ -5,8 +5,11 @@ const VillageModel = use('App/Models/Village')
 class VillageController {
   async index({ Request, response, view }) {
     const villages = await VillageModel.all()
+    const data = {
+      'title': 'Informasi Desa'
+    }
 
-    return response.json(villages.rows)
+    return view.render('village.index', { villages: villages.rows, data: data })
   }
 }
 

30d505e04aac72309dbcc80fe1587948e12a2a16
hafizulf <hafizulf68@gmail.com>
2021-10-03 17:23:33 +0700

    feat(auth): update logic for login and logout

--- app/Controllers/Http/Auth/LoginController.js
+++ app/Controllers/Http/Auth/LoginController.js
@@ -4,6 +4,22 @@ class LoginController {
   async index({ view }) {
     return view.render('auth.login', { title: 'Login Page' })
   }
+
+  async login({ request, response, session, auth }) {
+
+    const { email, password } = request.all()
+
+    await auth.attempt(email, password)
+
+    session.flash({ notification: 'Login berhasil' })
+    return response.route('village.index')
+  }
+
+  async logout({ response, auth }) {
+    await auth.logout()
+
+    return response.route('login.index')
+  }
 }
 
 module.exports = LoginController
